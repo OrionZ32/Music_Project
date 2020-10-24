be12749afab749de6756f15c6a178f0b38f10f7f
Harley Garcia <hagarcia99@outlook.com>
2020-10-24 14:47:03 -0500

    Mongoose connect and artist schema setup

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/musicAppDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const artistSchema = new mongoose.Schema({
+  artistName: String,
+  firstName: String,
+  lastName: String,
+  birthDate: Date,
+  dateSigned: Date,
+  contract: String,
+  //   discography: discographySchema,
+  netWorth: Number,
+  monthlyListeners: Number,
+  origin: String,
+  occupations: String,
+});
+
+const Artist = mongoose.model("Artist", artistSchema);
+
 app.get("/", function (req, res) {
   res.render("home");
 });
