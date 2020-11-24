61863956d4a9591d0b28145d795070d4fa41d68c
Harley Garcia <hagarcia99@outlook.com>
2020-11-24 11:12:10 -0600

    changing to new personal project

--- app.js
+++ app.js
@@ -5,44 +5,12 @@ const mongoose = require("mongoose");
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 mongoose.connect("mongodb://localhost:27017/musicAppDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-const discographySchema = new mongoose.Schema({
-  artistName: String,
-  collabArtistsName: String,
-  projectTitle: String,
-  projectType: String,
-  releaseDate: Date,
-  numberOfTracks: Number,
-  duration: Date,
-  rating: Number,
-  peakChartPosition: Number,
-  firstWeekSales: Number,
-  certificates: String,
-  label: String,
-  producers: String,
-});
-const artistSchema = new mongoose.Schema({
-  artistName: String,
-  firstName: String,
-  lastName: String,
-  birthDate: Date,
-  dateSigned: Date,
-  contract: String,
-  discography: discographySchema,
-  netWorth: Number,
-  monthlyListeners: Number,
-  origin: String,
-  occupations: String,
-});
-
-const Discography = mongoose.model("Discography", discographySchema);
-const Artist = mongoose.model("Artist", artistSchema);
 
 app.get("/", function (req, res) {
   res.render("home");
